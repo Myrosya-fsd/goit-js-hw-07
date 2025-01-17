08b5bdfb967c2af9a3541c2fe610254765b4b075
myroslava <murosja80@gmail.com>
2025-01-17 17:41:33 +0200

    hw_7

--- js/task-1.js
+++ js/task-1.js
@@ -1,36 +1,22 @@
-//Перед звільненням розробник зламав вихідний код управління акаунтами користувачів нашого сервісу доставки їжі. Виконай рефакторинг методів
-// об'єкта customer, розставивши відсутні this під час звернення до властивостей об'єкта.
+const categories = document.querySelectorAll(".item");
 
-//Використай цей стартовий код і виконай рефакторинг. Після оголошення об'єкта ми додали виклики методів. У консоль будуть виведені результати
-//  їх роботи. Будь ласка, нічого там не змінюй.
+console.log(`Number of categories: ${categories.length}`);
+for (const item of categories) {
+  console.log(`Category: ${item.querySelector("h2").textContent}`);
+  console.log(`Elements: ${item.querySelectorAll("li").length}`);
+}
 
-const customer = {
-  username: "Mango",
-  balance: 24000,
-  discount: 0.1,
-  orders: ["Burger", "Pizza", "Salad"],
-  // Change code below this line
-  getBalance() {
-    return this.balance;
-  },
-  getDiscount() {
-    return this.discount;
-  },
-  setDiscount(value) {
-    this.discount = value;
-  },
-  getOrders() {
-    return this.orders;
-  },
-  addOrder(cost, order) {
-    this.balance -= cost - cost * this.discount;
-    this.orders.push(order);
-  },
-  // Change code above this line
-};
+//------
 
-customer.setDiscount(0.15);
-console.log(customer.getDiscount()); // 0.15
-customer.addOrder(5000, "Steak");
-console.log(customer.getBalance()); // 19750
-console.log(customer.getOrders()); // ["Burger", "Pizza", "Salad", "Steak"]
+//const categories = document.querySelectorAll("#categories .item");
+
+//console.log(`Number of categories: ${categories.length}`);
+
+//categories.forEach((category) => {
+//  const categoryTitle = category.querySelector("h2").textContent;
+
+//  const itemsCount = category.querySelectorAll(".item-name .name").length;
+
+//  console.log(`Category: ${categoryTitle}`);
+//  console.log(`Elements: ${itemsCount}`);
+//});
